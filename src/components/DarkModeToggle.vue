7893c6d1559578ad3eb93ffee30626885e708865
Elif İrem Kara Kadyrov <83959541+elfrmkr@users.noreply.github.com>
2024-03-04 09:45:47 +0100

    feat: created theme toggle button

---
+++ src/components/DarkModeToggle.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="dark-mode-toggle" @click="toggleDarkMode">
+        <span v-if="isDarkMode">🌙</span>
+        <span v-else>☼</span>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            isDarkMode: true,
+        };
+    },
+    created() {
+        this.applyDarkMode(); // Apply dark mode when the component is created
+    },
+    methods: {
+        toggleDarkMode() {
+            this.isDarkMode = !this.isDarkMode;
+            this.applyDarkMode();
+        },
+        applyDarkMode() {
+            document.documentElement.classList.toggle('dark-mode', this.isDarkMode);
+        },
+    },
+};
+</script>
+
+<style scoped>
+/* Add necessary styles for the dark mode toggle */
+.dark-mode-toggle {
+    cursor: pointer;
+    font-size: 24px;
+    top: 1%;
+    right: 1%;
+    position: fixed;
+}
+</style>
